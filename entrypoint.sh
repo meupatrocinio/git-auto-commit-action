e0fadee92567cb00a54a34befee849c4fb0e1024
Patrick Almeida <patrick.almeida@meliuz.com.br>
2021-02-13 09:43:56 -0400

    Modify merge strategy to satisfy pipeline needs

--- entrypoint.sh
+++ entrypoint.sh
@@ -126,7 +126,7 @@ _merge_and_push_to_github() {
         git config user.email "${INPUT_COMMIT_USER_EMAIL}";
         git config user.name "${INPUT_COMMIT_USER_NAME}";
         git checkout --force "${INPUT_DEST_RELEASE_BRANCH}";
-        git merge --strategy=ours --ff --allow-unrelated-histories "${INPUT_BRANCH}";
+        git merge --strategy recursive --strategy-option theirs --ff --allow-unrelated-histories "${INPUT_BRANCH}";
         git push -f -u origin "${INPUT_DEST_RELEASE_BRANCH}";
         _push_to_github
     else
